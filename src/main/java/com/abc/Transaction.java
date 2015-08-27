b7b8daf31f7ce8e0fd140a0de05c3c2cddc4dca3
skerschbaumer <skersch@panix.com>
2015-08-26 21:40:49 -0400

    Update Transaction.java

--- src/main/java/com/abc/Transaction.java
+++ src/main/java/com/abc/Transaction.java
@@ -1,16 +1,30 @@
 package com.abc;
 
-import java.util.Calendar;
 import java.util.Date;
 
 public class Transaction {
-    public final double amount;
+	
+	public final double amount;
+    
+	private Date transactionDate;
 
-    private Date transactionDate;
+	public Transaction(double amount) {
+		this.amount = amount;
+		this.transactionDate = DateProvider.getInstance().now();
+	}
+	
+	public Date getTransactionDate() {
+		return this.transactionDate;
+	}
+	
+	public void setTransactionDate(Date date) {
+		this.transactionDate = date;
+	}
 
-    public Transaction(double amount) {
-        this.amount = amount;
-        this.transactionDate = DateProvider.getInstance().now();
+	public String toString() {
+		StringBuffer buffer = new StringBuffer();
+		buffer.append("[ amount:" + amount + "," + "date:" + transactionDate + "]");
+		return buffer.toString();
     }
-
+    
 }
